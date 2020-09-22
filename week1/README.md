1b3a00c52bda7bf2127773d8edb41bcb06ddd892
yawenhu <yawenhu@umich.edu>
2020-09-22 10:52:03 -0400

    brief description on week1 activities

---
+++ week1/README.md
@@ -0,0 +1,3 @@
+# Description:
+week1_part1.sh: 
+week2_part2_args.sh:
---
+++ week1/week1_part2_args.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+# Stats 506, Fall 2020
+# This script serves as an illustration for how to use
+# arguments passed at the command line. 
+#
+# Run as below:
+#  bash ./week1_part2_args.sh file_name pattern
+#
+# Author(s): James Henderson
+# Updated: September 8, 2020
+# 79: -------------------------------------------------------------------------
+
+echo $1
+echo $2
+
+# 79: -------------------------------------------------------------------------
